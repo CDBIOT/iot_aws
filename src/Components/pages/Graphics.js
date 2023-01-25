4642c2762ac047a3e69d72b54db0817d11b0f983
Cristiano Bento <crisdb2008@gmail.com>
2023-01-25 12:29:18 -0300

    //

--- src/Components/pages/Graphics.js
+++ src/Components/pages/Graphics.js
@@ -14,7 +14,7 @@ function Graphics(){
     return temps.filter(item=> item.dia.includes(query))
     }
     
-console.log(temps[i].filter(temp=>temp.includes(6)))
+//console.log(temps[i].filter(temp=>temp.includes(6)))
 
  const dias = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
 
