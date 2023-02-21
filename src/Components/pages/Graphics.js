e364ddec684e6ac8ea15dac1a4cd57466bbc01aa
Cristiano Bento <crisdb2008@gmail.com>
2023-02-21 18:33:35 -0300

    date

--- src/Components/pages/Graphics.js
+++ src/Components/pages/Graphics.js
@@ -62,11 +62,11 @@ return (
 <th colspan = {4}> <h1> Selecione o período </h1></th>
 <tr>
     <td>
-        <h2 value = {startDate}  size="6" /><h2 for="initDate" className="label">Data início: {startDate}</h2>
+        <h2 for="initDate" className="label">Data início:  {startDate}</h2>
         <input id="initDate" value={""}  type="date" onChange={(e)=>setInitDate(e.target.value)}  name="initDate" /></td>
         <td> </td>
     <td>
-        <h2 value = {endDate}  size="6" /> <h2 for="endDate" className="label">Data Final: {endDate} </h2>
+        <h2 for="endDate" className="label">Data Final:  {endDate} </h2>
         <input id="finalDate" value={""}  type="date" onChange={(e)=>setFinalDate(e.target.value)}  name="finalDate" /></td>
         <td></td> 
 </tr>
