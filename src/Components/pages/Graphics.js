b8d79027b712a51453b01218a0caf773d756d3ea
Cristiano Bento <crisdb2008@gmail.com>
2023-02-21 18:12:26 -0300

    td

--- src/Components/pages/Graphics.js
+++ src/Components/pages/Graphics.js
@@ -65,12 +65,12 @@ return (
 <tr>
     <td>
         <h2>Data início</h2>
-        <input id="initDate" value={""}  type="date" onChange={(e)=>setInitDate(e.target.value)}  name="initDate" /></td>
-        <h2 value = {startDate}  size="6" /><h2 for="initDate" className="form-label">{startDate}</h2>
+        <input id="initDate" value={""}  type="date" onChange={(e)=>setInitDate(e.target.value)}  name="initDate" />
+        <h2 value = {startDate}  size="6" /><h2 for="initDate" className="form-label">{startDate}</h2></td>
     <td>
         <h2>Data Final</h2>
-        <input id="finalDate" value={""}  type="date" onChange={(e)=>setFinalDate(e.target.value)}  name="finalDate" /></td> 
-        <h2  value = {endDate}  size="6" /> <h2 for="endDate" className="form-label">{endDate} </h2>
+        <input id="finalDate" value={""}  type="date" onChange={(e)=>setFinalDate(e.target.value)}  name="finalDate" />
+        <h2  value = {endDate}  size="6" /> <h2 for="endDate" className="form-label">{endDate} </h2></td> 
 </tr>
 </table>
    
