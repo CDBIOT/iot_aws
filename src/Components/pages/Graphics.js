1858fbe1ebb44acd7b285b2b5d7c58e6f7576d6a
Cristiano Bento <crisdb2008@gmail.com>
2023-02-22 17:29:54 -0300

    filtro

--- src/Components/pages/Graphics.js
+++ src/Components/pages/Graphics.js
@@ -17,7 +17,7 @@ function Graphics(){
     const dias = temps.map((t, i) =>{return temps.indexOf(t.dia)===i})
 
     const filtrodias = temps.filter((item)=> {
-        return item[temps.length].includes(27)
+        return item[temps.length]>=10
     })
 
 function set(){
