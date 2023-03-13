c794d1460e1df2a8648aabc9d9f587f3bb51419e
Cristiano Bento <crisdb2008@gmail.com>
2023-03-13 20:29:08 -0300

    iot

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react';
 import {BrowserRouter as Router, Route , Routes} from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import {Amplify}  from 'aws-amplify';
+import Amplify, {API}  from 'aws-amplify';
 import {withAuthenticator} from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 import config from './aws-exports'
@@ -19,23 +19,13 @@ Amplify.configure(config)
 
 function App() {
 
-    
-  // useEffect(() => {
-  
-  //   API.get("serverAwsIot","/dev/temps")
-  //    .then((response)=> {(response.data.temps);
-  //   console.log(response.data)
-  //   });
-
-  // }, []);
-
 return (
   <div className = "App" > 
  <Router>
     <Navbar />
   
          <Routes>
-            <Route exact path="/"   element= {<Home />}></Route>
+            <Route exact path="/"   element={<Home />}></Route>
             <Route path="/Energy"   element={<Energy />}></Route>
             <Route path='/Graphics' element={<Graphics />}></Route>
             <Route path="/Schedule" element={<Schedule />}></Route>
